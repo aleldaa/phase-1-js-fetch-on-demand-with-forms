51aa384c26ffd8c54c9c72c9a5fe91a35596be8b
Luna <aleldaproductions@gmail.com>
2023-03-25 19:36:02 -0500

    finished codealong

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+    //selected the location to create event
+  const inputForm = document.querySelector('form')
+    //adds the event
+  inputForm.addEventListener('submit', (event) => {
+    //prevents website from resetting
+    event.preventDefault()
+    //getting the value we need
+    const input = document.querySelector("input#searchByID")
+
+    console.log(input.value)
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.innerText = data.title;
+            summary.innerText = data.summary
+        })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
